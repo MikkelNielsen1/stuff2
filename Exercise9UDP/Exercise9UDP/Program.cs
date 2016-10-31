0406be38d08a3e9807f8cd3678086d95334647d6
winslowism <mpkonstmann@hotmail.com>
2016-10-31 14:56:03 +0100

    klsajdhaksjdhk

--- Exercise9UDP/Exercise9UDP/Program.cs
+++ Exercise9UDP/Exercise9UDP/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.IO;
 
 namespace Exercise9UDP
 {
 	class MainClass
 	{
 		const int PORT = 9000;
-		public byte[] ipAddress = new byte[] {10, 0, 0, 1};
-		public byte[] message;
+		public static byte[] ipAddress = new byte[] {10, 0, 0, 1};
+		public static byte[] message;
+		public static string readFileData;
+		public static string messagestring;
 
 
 		public static void Main (string[] args)
@@ -17,15 +21,36 @@ namespace Exercise9UDP
 			IPEndPoint ep = new IPEndPoint (new IPAddress (ipAddress), PORT);
 			UdpClient client = new UdpClient (ep);
 
-			IPEndPoint senderinfo = new IPEndPoint (IPAddress.Any, PORT);
+			IPEndPoint senderinfo = new IPEndPoint (IPAddress.Any, 0);
 			while (true) 
 			{
 				Byte[] receiveBytes = client.Receive (ref senderinfo); 	
 
-				Console.WriteLine (senderinfo.Address + " " + senderinfo.Port);
-				string returnData = Encoding.ASCII.GetString(receiveBytes);
 
-				client.Send (message, sizeof(message), senderinfo);
+				string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+
+				Console.WriteLine (receivedData);
+				if (receivedData.ToLower () == "u") 
+				{
+					readFileData = File.ReadAllText ("/proc/uptime");
+					messagestring = "Uptime is " + readFileData;
+					message = Encoding.ASCII.GetBytes (messagestring);
+
+						
+				} else if (receivedData.ToLower () == "l") 
+				{
+					readFileData = File.ReadAllText ("/proc/loadavg");
+					messagestring = "Load average is " + readFileData;
+					message = Encoding.ASCII.GetBytes (messagestring);
+				} else 
+				{
+					messagestring = "Invalid character passed";
+					message = Encoding.ASCII.GetBytes (messagestring);
+				}
+
+				client.Send (message, message.Length, senderinfo);
+
 			}
 
 
